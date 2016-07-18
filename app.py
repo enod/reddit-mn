7b49043a13392905f28f7a904661c9e39b110c22
enod <enod2008@gmail.com>
2016-07-18 17:47:31 +0800

    Add login-required and logged-in landing page for user

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
 app = Flask(__name__)
+app.secret_key = 'temporary key'
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash(u'Нэвтрэн орно уу?')
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('public_home.html')
+
+
+@app.route('/user_home')
+@login_required
+def user_home():
+    return render_template('user_home.html')
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != "admin" or request.form["password"] != "admin":
+            error = u'Алдаа гарлаа. Шалгаад дахин нэвтэрнэ үү?'
+        else:
+            session['logged_in'] = True
+            flash(u'Амжилттай нэвтэрлээ!')
+            return redirect(url_for('user_home'))
+    return render_template("login.html", error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
